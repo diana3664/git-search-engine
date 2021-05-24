97c25dfdc7697c471b5216c858efa65c044c7f6c
diana3664 <dianamus2017@gmail.com>
2021-05-24 21:16:17 +0300

    Auto-generated commit

--- src/app/repository.ts
+++
@@ -1,4 +0,0 @@
-export class Repository {
-  constructor(public repoId: number,public RepoName: string, public picture: string,public description:string ,public language: number, public forks: number){}
-
-}
